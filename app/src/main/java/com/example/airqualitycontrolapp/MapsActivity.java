2acacb24e46fd6ac3fe0de1c94030029c72da6c8
SunJoJ <andrzejtawrel1998@gmail.com>
2020-02-15 14:01:42 +0100

    Customized bottom app bar

--- app/src/main/java/com/example/airqualitycontrolapp/MapsActivity.java
+++ app/src/main/java/com/example/airqualitycontrolapp/MapsActivity.java
@@ -8,6 +8,10 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -31,6 +35,30 @@ public class MapsActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mPager);
 
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.bottom_appbar_menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.app_bar_fav:
+//                Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.app_bar_search:
+//                Toast.makeText(getApplicationContext(), "2", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.app_bar_settings:
+//                Toast.makeText(getApplicationContext(), "3", Toast.LENGTH_SHORT).show();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
 
     @Override
